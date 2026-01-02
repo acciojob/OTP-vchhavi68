2b6f6b653ed9abe7effbf221767b81db0e7cfa1c
vchhavi68 <vchhavi68@gmail.com>
2026-01-02 12:22:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', () => {
+    if (!/^\d$/.test(input.value)) {
+      input.value = '';
+      return;
+    }
+
+    if (index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (input.value === '' && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = '';
+      }
+    }
+  });
+});
